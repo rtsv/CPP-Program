cf03ba4c50e7d40550d286ad4af1dc58b29b1045
rtsv <92084142+rtsv@users.noreply.github.com>
2021-12-12 22:22:42 +0530

    Create rev_array.cpp

---
+++ rev_array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void reverse(int a[], int n)
+{
+    int j;
+    for(int i=0, j=n-1; i<n/2; i++, j--)
+    {
+        swap(a[i], a[j]);
+    }
+    
+}
+
+int main(){
+    
+    int a[]={5,6,3,4,9,2};
+    int n = sizeof(a)/sizeof(int);
+    reverse(a,n);
+    for(int x:a)
+    {
+        cout<<x<<" ";
+    }
+    
+}
